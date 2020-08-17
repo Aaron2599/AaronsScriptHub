2b7078fdbc08fdd0cbf42b09ff096865fee9174f
Aaron2599 <61896509+Aaron2599@users.noreply.github.com>
2020-08-17 07:00:46 -0400

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,6 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/machport/machport.github.io/master/LightSpeedcyb.lua"))()
+
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/machport/machport.github.io/master/LightSpeedcyb.lua"))()
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/MicrosoftThawne/UnNamed-Esp/master/unnamed/unnamed.lua'))()
 
