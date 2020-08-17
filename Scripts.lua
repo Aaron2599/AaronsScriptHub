3acb145ca133a9c6ab8fc1ef7b3b2624876f1bfe
Aaron2599 <61896509+Aaron2599@users.noreply.github.com>
2020-08-17 07:08:12 -0400

    added faster loading script

--- Scripts.lua
+++ Scripts.lua
@@ -2,7 +2,7 @@
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/machport/machport.github.io/master/LightSpeedcyb.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/LightSpeedcyb.lua"))()
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/MicrosoftThawne/UnNamed-Esp/master/unnamed/unnamed.lua'))()
 
