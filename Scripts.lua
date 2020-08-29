87063fa73489163b530b0ff11eb9b13a645b923f
Aaron2599 <61896509+Aaron2599@users.noreply.github.com>
2020-08-29 19:35:27 -0400

    added big paintball script

--- Scripts.lua
+++ Scripts.lua
@@ -18,6 +18,18 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScrip
 -- Game Specific Hacks
 if game.Workspace.SCP662_Bell then
 loadstring(game:HttpGetAsync("https://gist.githubusercontent.com/GuzZyWilly/622836d5371a580b5bbf9e119f9de150/raw/0be92f374888ccc19cf47503b7b2504f72a54262/gistfile1.txt"))()
+else
+
+if game.Workspace.__THINGS.__REMOTES then
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/BIG_Paintball_AIMBOT_GUI.txt'))()
+else
+   
+  
+  
+  
+  
+  
+  end
 end
 
 
