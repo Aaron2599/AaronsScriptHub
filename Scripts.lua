8801b44b371f303f49626ed55ea9c3474b97004e
Aaron2599 <61896509+Aaron2599@users.noreply.github.com>
2020-08-28 20:57:59 -0400

    changed loading order

--- Scripts.lua
+++ Scripts.lua
@@ -1,12 +1,12 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/Spy_Chat.lua'))()
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/LightSpeedcyb.lua"))()
 
 loadstring(game:HttpGet('https://ic3w0lf.xyz/rblx/protoesp.lua'))()
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/aimbot.lua'))()
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
 
 
