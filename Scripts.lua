1abd2c224cc504360ff183039004f16131dd4c75
Aaron2599 <61896509+Aaron2599@users.noreply.github.com>
2020-08-30 15:41:57 -0400

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -1,12 +1,7 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/Spy_Chat.lua'))()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/LightSpeedcyb.lua"))()
-
-loadstring(game:HttpGet('https://ic3w0lf.xyz/rblx/protoesp.lua'))()
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/aimbot.lua'))()
+local esp = 1 
+local aim = 1 
+local infiniteyield = 1
 
 
 
@@ -16,20 +11,38 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScrip
 
 
 -- Game Specific Hacks
-if game.Workspace.SCP662_Bell then
+if game.PlaceId == 5323592216  then -- Site 67
 loadstring(game:HttpGetAsync("https://gist.githubusercontent.com/GuzZyWilly/622836d5371a580b5bbf9e119f9de150/raw/0be92f374888ccc19cf47503b7b2504f72a54262/gistfile1.txt"))()
-else
 
-if game.Workspace.__THINGS.__REMOTES then
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/BIG_Paintball_AIMBOT_GUI.txt'))()
-else
+
+end
+
+if game.PlaceId == 3527629287 then -- BIG_Paintball
+loadstring(game:HttpGet("https://www.pastebin.com/raw/PuaaGKsT"))()
+ esp = 0
+ aim =  0
+end
    
   
   
-  
-  
-  
-  end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/Spy_Chat.lua'))()
+
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/LightSpeedcyb.lua"))()
+
+
+if esp == 1 then 
+loadstring(game:HttpGet('https://ic3w0lf.xyz/rblx/protoesp.lua'))()
+end 
+
+if aim ==1 then 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Aaron2599/AaronsScriptHub/master/aimbot.lua'))()
+ end
+
+
+  if infiniteyield == 1 then 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end
 
 
